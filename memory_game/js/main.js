ee08302f156bb1d4937b91fdb5ec7924703b212c
Philip Hoover <philhoov90@gmail.com>
2018-08-28 13:07:51 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = 'King'
+var cardTwo = 'Queen'
+var cardThree = 'King'
+var cardFour = 'Queen'
+console.log("User Flipped " + cardOne)
+console.log("User Flipped " + cardTwo)
