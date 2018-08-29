4b8db7af7de4eacce5067e8219b080c8da64511d
Philip Hoover <philhoov90@gmail.com>
2018-08-29 13:19:20 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-console.log("Up and running!");
-var cardOne = 'King'
-var cardTwo = 'Queen'
-var cardThree = 'King'
-var cardFour = 'Queen'
-console.log("User Flipped " + cardOne)
-console.log("User Flipped " + cardTwo)
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[0];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardTwo);
+
