db58d4afaa8a03178ef69eac95c881ee11220e7b
Philip Hoover <philhoov90@gmail.com>
2018-09-02 22:19:27 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,42 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{cardImage:"queen", front: "images/queen-of-diamonds.png" }, 
+{cardImage:"king", front: "images/king-of-hearts.png"},
+{cardImage:"queen", front: "images/queen-of-hearts.png" }, 
+{cardImage:"king", front: "images/king-of-diamonds.png"}
+]
+
 var cardsInPlay = [];
 
-var checkForMatch = function (cardId) {
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!");
-} 
-else {
+var flipCard = function() {
+  var cardId = this.getAttribute("data-id");
+  console.log("User flipped " + cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].cardImage);
+  this.setAttribute("src", cards[cardId].front);
+}
+
+var createBoard = function() {
+  var board = document.getElementById("game-board");
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img"); 
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    board.appendChild(cardElement);
+}
+}
+
+var checkForMatch = function (cardsInPlay) {
+ if (cardsInPlay.length === 2 && cardsInPlay[0].cardImage === cardsInPlay[1].cardImage) {
+ 	alert("You found a match!");
+} else {
 	alert("Sorry, try again.");
-} 
+ }
 }
+//I could not figure out how to get the checkForMatch function to display the alert without refreshing the page... no errors. wtf
 
-var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId])
-}
+createBoard();
+checkForMatch(cardsInPlay);
 
-flipCard(0);
-flipCard(2);
-flipCard(checkForMatch(0));
+
